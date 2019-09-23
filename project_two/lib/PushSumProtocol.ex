8de81e39966608a02a0deca53913caae7eeb496b
Aotian Wu <aotianwu@Aotians-MacBook-Pro.local>
2019-09-23 14:20:30 -0400

    first version

--- project_two/lib/PushSumProtocol.ex
+++ project_two/lib/PushSumProtocol.ex
@@ -1,27 +1,43 @@
+# create all the nodes
 defmodule PushSumProtocol do
-  def main(totalNodes, topology) do
-    IO.puts("Total nodes are #{Integer.to_string(totalNodes)}");
-    # IO.puts("The required topology is #{topology}");
+  use GenServer
 
-    # Selecting the topology and then re-directing to the required function.
-    # We are also initializing the actors as requested by the user.
-
-    case topology do
-      "full" ->
-        IO.puts("You have selected the full topology for actor connections in Push-Sum Protocol")
-      "line" ->
-        IO.puts("You have selected the line topology for actor connections in Push-Sum Protocol")
-      "rand2D" ->
-        IO.puts("You have selected the random2D topology for actor connections in Push-Sum Protocol")
-      "3Dtorus" ->
-        IO.puts("You have selected the 3DTorus topology for actor connections in Push-Sum Protocol")
-      "honeycomb" ->
-        IO.puts("You have selected the honeycomb topology for actor connections in Push-Sum Protocol")
-      "randhoneycomb" ->
-        IO.puts("You have selected the random honeycomb topology for actor connections in Push-Sum Protocol")
-      _ ->
-        IO.puts("You have selected an incorrect topology for actor connections in Push-Sum Protocol")
-        System.halt(0);
-    end
+  def start_link(topology, node_num) do
+    GenServer.start_link(__MODULE__, [topology, node_num])
   end
+
+  def init(args) do
+    [topology, node_num] = args
+    ppid = self()
+    pid_list = Enum.map(1..node_num, fn node_id -> 
+      {:ok, pid} = Actor.start_link(node_id, 1.0, ppid)
+      pid end)
+
+    {:ok, neighbor_pid} = Neighbor.start_link(pid_list, topology)
+    for pid <- pid_list do
+      nbs = Neighbor.get_neighbors(neighbor_pid, pid)
+      Actor.set_neighbors(pid, nbs)
+    end
+    {:ok, [topology, pid_list]}
+  end
+
+  def start(pid) do
+    GenServer.call(pid, :start, :infinity)
+  end
+
+  def handle_call(:start, _from, state) do
+    [_, pid_list] = state
+    
+    # start gossip
+    node_pid = Enum.random(pid_list)
+    IO.inspect(node_pid)
+    Actor.recieve(node_pid, 0, 0)
+    receive do
+      :finish -> :ok
+    end  
+
+    {:reply, [], state}
+  end
+
+
 end
---
+++ project_two/lib/node.ex
@@ -0,0 +1,54 @@
+defmodule Actor do
+    use GenServer
+
+    def start_link(n, w, ppid) do
+        GenServer.start_link(__MODULE__, [n: n, w: w, nbs: [], cnt: 0, ppid: ppid]) # nbs is the list of neighbors' pids
+    end
+
+    def init(args) do
+        {:ok, args} 
+    end
+
+    def set_neighbors(pid, nbs) do
+        IO.inspect(pid)
+        IO.inspect(nbs)
+        GenServer.cast(pid, {:neighbor, nbs})
+    end
+
+    def recieve(pid, in_n, in_w) do
+        IO.puts ("recieved #{in_n}, #{in_w}")
+        IO.inspect(pid)
+        :ok = GenServer.cast(pid, {:recieve, in_n, in_w}) 
+        IO.puts("ok")
+    end
+
+    def handle_cast({:neighbor, nb_list}, state) do
+        [n: n, w: w, nbs: nbs, cnt: cnt, ppid: ppid] = state
+        {:noreply, [n: n, w: w, nbs: nb_list, cnt: cnt, ppid: ppid]}
+    end
+
+    def handle_cast({:recieve, in_n, in_w}, state) do
+        IO.inspect(state)
+        [n: n, w: w, nbs: nbs, cnt: cnt, ppid: ppid] = state
+        old_ratio = n/w
+        [new_n, new_w] = [n + in_n, w + in_w]
+        new_ratio = new_n / new_w
+        cnt = if abs(new_ratio - old_ratio) < 1.0e-10, do: cnt + 1, else: 0
+        if (cnt >= 3) do
+            IO.puts("The ratio #{new_ratio} hasn't been changed for 3 iterations")
+            send(ppid, :finish)
+            {:noreply, []}
+        else
+            IO.inspect(state)
+            nb = Enum.random(nbs)
+            :timer.sleep(Enum.random(0..10)); #TODO==================
+            Actor.recieve(nb, new_n/2, new_w/2)     
+
+            {:noreply, [n: new_n/2, w: new_w/2, nbs: nbs, cnt: cnt, ppid: ppid]}
+        end
+    end
+
+
+
+
+end
